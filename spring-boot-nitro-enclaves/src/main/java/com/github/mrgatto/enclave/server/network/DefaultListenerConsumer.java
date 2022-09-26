5b349c85c678089af8de3d9caec37e3a3ac03844
dahuahua <38651850@qq.com>
2022-09-26 12:17:51 +0800

    [longzhihun] feat: add close connection failure

--- spring-boot-nitro-enclaves/src/main/java/com/github/mrgatto/enclave/server/network/DefaultListenerConsumer.java
+++ spring-boot-nitro-enclaves/src/main/java/com/github/mrgatto/enclave/server/network/DefaultListenerConsumer.java
@@ -69,7 +69,7 @@ public class DefaultListenerConsumer implements ListenerConsumer, ApplicationLis
 		} finally {
 			IOUtils.closeQuietly(in);
 			IOUtils.closeQuietly(out);
-			IOUtils.closeQuietly(connection);
+			connection.close();
 		}
 	}
 
